7a50a901c2680efe701e3ad2ae5935d0678dc8bb
unknown <394229785@qq.com>
2016-06-16 19:52:25 +0800

    edit the exports

--- dist/task.js
+++ dist/task.js
@@ -1,14 +1,5 @@
-;(function(){
-var task = {};
-
-
-if (typeof module != "undefined" && module.exports) {
-    module.exports = task;
-}
-else {
-    window["task"] = task;
-}
-task.callbacks = function () {
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var callbacks = function () {
 
     var list = [],        // 回调数组
         fireIndex = -1,   // 当前正在回调的索引
@@ -60,8 +51,8 @@ task.callbacks = function () {
         var item = list[fireIndex++],
             args = [].slice.call(arguments);
 
-        if (fireType == 'default') {
-            args = params.slice(); 
+        if (statu == 'default') {
+            args = params.slice();
         }
 
         if (!item) return;
@@ -75,28 +66,54 @@ task.callbacks = function () {
     };
 };
 
-var utils = {
-    getType: function (sender) {
-        return sender === null ? (sender + '') : Object.prototype.toString.call(sender).toLowerCase().match(/\s(\S+?)\]/)[1];
-    },
-    each: function (sender, callback) {
-        var i = 0,                      // 循环用变量
-            len = sender.length,           // 长度
-            arrayLike = this.arrayLike(sender); // 是否属于(类)数组
 
-        if (arrayLike) {
-            for (; i < len; i++) {
-                callback.call(sender[i], i, sender[i]);
-            }
-        } else {
-            for (i in sender) {
-                callback.call(sender[i], i, sender[i]);
-            }
-        }
-    },
-    arrayLike: function (sender) {
-        // duck typing ，检测是否属于数组
-        return typeof sender.length == 'number' && typeof sender.splice == 'function';
+module.exports = callbacks;
+
+
+},{}],2:[function(require,module,exports){
+var task = {};
+
+module.exports = task;
+},{}],3:[function(require,module,exports){
+module.exports = function (task) {
+    if (typeof define === "function" && define.amd) {
+        define("task", [], function () {
+            return task;
+        });
     }
-};
-})();
+}
+},{}],4:[function(require,module,exports){
+var amd = require('./amd');
+var global = require('./global');
+
+module.exports = function (task) {
+    amd(task);
+    global(task);
+}
+},{"./amd":3,"./global":5}],5:[function(require,module,exports){
+module.exports = function (task) {
+    if (typeof window !== 'undefined') {
+        var _task = window.task;
+
+        window.task = task;
+
+        task.noConflict = function () {
+            window.task = _task;
+            return task;
+        };
+    }
+}
+},{}],6:[function(require,module,exports){
+var task = require('./core');
+
+task.callbacks = require('./callbacks');
+
+
+
+// require('./queue'); 
+
+// 适配 amd 模式， window 环境
+require('./exports/exports')(task);
+
+module.exports = task;
+},{"./callbacks":1,"./core":2,"./exports/exports":4}]},{},[6]);
