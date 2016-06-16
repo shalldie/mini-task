d7804a4aa2c64d59139af8ea42d7ae80186dae1e
unknown <394229785@qq.com>
2016-06-16 20:09:35 +0800

    test the file app.js

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-var task = require('./dist/task');
+var task = require('./src/task');
 
-var cb = task.callbacks('queue');
-
-cb.add(function (next) {
-    console.log(arguments);
-    setTimeout(function () {
-        next(12, 'tom');
-    },1000)
-});
-
-cb.add(function (next) {
-    console.log(arguments);
-    next();
-});
-
-cb.fire('tom', 12);
+console.log(task.callbacks);
