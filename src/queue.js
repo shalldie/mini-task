b19ab96ef782793fe4cb0feb4327376bfb2327a0
unknown <394229785@qq.com>
2016-06-20 00:39:40 +0800

    add the queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,7 +1,38 @@
-require('./callback');
-
 var queue = function () {
-    console.log('queue');
-} 
- 
+    var callbacks = [],
+        args = [],
+        nowIndex = -1,
+        notify = function () { };
+
+    function next() {
+        nowIndex++;
+        if (nowIndex >= callbacks.length) {
+            notify();
+            return;
+        }
+
+        args = [].slice.call(arguments);
+        callbacks[nowIndex]();
+    }
+
+    function add(cb) {
+        callbacks.push(function () {
+            args.unshift(next);
+            cb.apply(null, args);
+        });
+    }
+
+    function fire() {
+        next.apply([], arguments);
+    }
+
+    return {
+        add: add,
+        fire: fire,
+        notify: function () {
+            notify = arguments[0] || function () { };
+        }
+    }
+};
+
 module.exports = queue;
