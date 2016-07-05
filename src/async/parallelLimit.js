53a06d5768accdd24d37942c65320e2d73681f27
unknown <394229785@qq.com>
2016-07-05 18:44:14 +0800

    edit parallelLimit

--- src/async/parallelLimit.js
+++ src/async/parallelLimit.js
@@ -6,16 +6,27 @@ task.parallelLimit = function (actions, maxNum, callback) {
         len = actions.length,
         argsArr = new Array(len),  // 用于存储返回值
         dfd = task.deferred(),     // 用于当前操作的deferred
+        queues = [],
         xxx;
+
+    function ifDone() {  // 检测是否完成
+        return num == 0 && nowIndex >= len;
+    }
 
     var control = function () {
         // 释放队列
-        if (num < maxNum && nowIndex + 1 < len) {
+        for (; nowIndex < len && max - num > 0; nowIndex++) {
+            num++;
+            queues[nowIndex].dequeue();
+        }
 
+        // 如果完成
+        if (ifDone()) {
+            dfd.resolve();
         }
     };
 
-    actions.map(function (act, i) {
+    queues = actions.map(function (act, i) {
         var queue = task.queue();
         queue.queue(act);                       // 执行方法
         queue.queue(function (next) {           // 处理数据
@@ -24,10 +35,15 @@ task.parallelLimit = function (actions, maxNum, callback) {
             argsArr[i] = args;
             num--; next();
         });
+        queue.catch(function (err) {
+            dfd.reject(err);
+        });
+
         queue.queue(function (next) {            // 检测，后续处理
 
             next();
-        })
+        });
+        return queue;
     });
 
 
