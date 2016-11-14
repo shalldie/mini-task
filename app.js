02b77e4609412e685fedc170db04bd1aebd0a6cf
谢爽 <394229785@qq.com>
2016-11-14 14:15:44 +0800

    修改测试方式

--- app.js
+++ app.js
@@ -5,57 +5,61 @@ let task = require('./index');
 
 // callbacks
 
-console.log('------------------------callbacks');
-let cb = task.callbacks();
+function testCallbacks() {
 
-cb.add(n => console.log(n));
-cb.add(n => console.log(n + n));
+    console.log('------------------------callbacks');
+    let cb = task.callbacks();
 
-cb.fire(1);
+    cb.add(n => console.log(n));
+    cb.add(n => console.log(n + n));
 
-
-console.log('------------------------callbacks-memory');
-cb = task.callbacks('memory');
-
-cb.add(n => console.log(n));
-
-cb.fire(1);
-
-cb.add(n => console.log(n + n));
+    cb.fire(1);
 
 
-console.log('------------------------callbacks-stopOnFalse');
-cb = task.callbacks('stopOnFalse');
+    console.log('------------------------callbacks-memory');
+    cb = task.callbacks('memory');
 
-cb.add(n => (console.log(n), false));
+    cb.add(n => console.log(n));
 
-cb.add(n => console.log(n + n));
+    cb.fire(1);
 
-cb.fire(1);
+    cb.add(n => console.log(n + n));
 
 
-console.log('------------------------callbacks-once');
-cb = task.callbacks('once');
+    console.log('------------------------callbacks-stopOnFalse');
+    cb = task.callbacks('stopOnFalse');
 
-cb.add(n => (console.log(n), false));
+    cb.add(n => (console.log(n), false));
 
-cb.add(n => console.log(n + n));
+    cb.add(n => console.log(n + n));
 
-cb.fire(1);
-cb.fire(2);
+    cb.fire(1);
 
-console.log('------------------------callbacks-once-memory');
-cb = task.callbacks('once memory');
 
-cb.add(n => (console.log(n), false));
+    console.log('------------------------callbacks-once');
+    cb = task.callbacks('once');
 
-cb.add(n => console.log(n + n));
+    cb.add(n => (console.log(n), false));
 
-cb.fire(1);
-cb.fire(2);
+    cb.add(n => console.log(n + n));
 
-cb.add(n => console.log(n * 3));
+    cb.fire(1);
+    cb.fire(2);
 
+    console.log('------------------------callbacks-once-memory');
+    cb = task.callbacks('once memory');
+
+    cb.add(n => (console.log(n), false));
+
+    cb.add(n => console.log(n + n));
+
+    cb.fire(1);
+    cb.fire(2);
+
+    cb.add(n => console.log(n * 3));
+}
+
+testCallbacks();
 
 
 
